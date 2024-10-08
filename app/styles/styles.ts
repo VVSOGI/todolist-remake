e3464266cfc06e2c90dc4b59f33ced4179a79d11
benny <vvsogi@gmail.com>
2024-10-08 18:00:12 +0900

    Add fonts and styles for the page

---
+++ app/styles/styles.ts
@@ -0,0 +1,32 @@
+import { colors } from "./colors";
+
+export const styles = {
+  mainColor: {
+    primary: colors.red_500,
+    secondary: colors.red_300,
+  },
+
+  backgroundColor: {
+    primary: colors.cloudBlue_300,
+    default: colors.white,
+  },
+
+  boxShadow: {
+    primary: "0 4px 8px rgba(0, 0, 0, 0.2)",
+  },
+
+  buttons: {
+    hover: "#f1f1f1",
+    active: "#fbfbfb",
+  },
+
+  borderColor: {
+    primary: colors.gray_200,
+  },
+
+  borderRadius: {
+    small: "4px",
+    medium: "8px",
+    large: "12px",
+  },
+};
