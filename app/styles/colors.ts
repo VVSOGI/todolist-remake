f2f64536c62d1ffc2d2d2b8bd497feb085d9e1bb
benny <vvsogi@gmail.com>
2024-10-10 17:18:29 +0900

    Update colors and styles

--- app/styles/colors.ts
+++ app/styles/colors.ts
@@ -1,22 +1,22 @@
 export const colors = {
-  red_100: "#fcc7c5",
-  red_200: "#e89b99",
-  red_300: "#e67775",
-  red_400: "#e85856",
-  red_500: "#ec4442",
+  red_100: '#fcc7c5',
+  red_200: '#e89b99',
+  red_300: '#e67775',
+  red_400: '#e85856',
+  red_500: '#ec4442',
 
-  gray_100: "#e6e3e3",
-  gray_200: "#bababa",
-  gray_300: "#969696",
-  gray_400: "#5c5c5c",
-  gray_500: "#363636",
+  gray_100: '#f7f7f7',
+  gray_200: '#bababa',
+  gray_300: '#969696',
+  gray_400: '#5c5c5c',
+  gray_500: '#363636',
 
-  cloudBlue_100: "#d7dce6",
-  cloudBlue_200: "#c3cee3",
-  cloudBlue_300: "#a3bae3",
-  cloudBlue_400: "#86a8e3",
-  cloudBlue_500: "#608fe0",
+  cloudBlue_100: '#d7dce6',
+  cloudBlue_200: '#c3cee3',
+  cloudBlue_300: '#a3bae3',
+  cloudBlue_400: '#86a8e3',
+  cloudBlue_500: '#608fe0',
 
-  white: "#ffffff",
-  black: "#000000",
-};
+  white: '#ffffff',
+  black: '#000000'
+}
--- app/styles/styles.ts
+++ app/styles/styles.ts
@@ -1,32 +1,32 @@
-import { colors } from "./colors";
+import { colors } from './colors'
 
 export const styles = {
   mainColor: {
     primary: colors.red_500,
-    secondary: colors.red_300,
+    secondary: colors.red_300
   },
 
   backgroundColor: {
     primary: colors.cloudBlue_300,
-    default: colors.white,
+    default: colors.white
   },
 
   boxShadow: {
-    primary: "0 4px 8px rgba(0, 0, 0, 0.2)",
+    primary: '0 4px 8px rgba(0, 0, 0, 0.2)'
   },
 
   buttons: {
-    hover: "#f1f1f1",
-    active: "#fbfbfb",
+    hover: colors.gray_100,
+    active: '#f2f2f2'
   },
 
   borderColor: {
-    primary: colors.gray_200,
+    primary: colors.gray_200
   },
 
   borderRadius: {
-    small: "4px",
-    medium: "8px",
-    large: "12px",
-  },
-};
+    small: '4px',
+    medium: '8px',
+    large: '12px'
+  }
+}
