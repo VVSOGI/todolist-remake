4eda70835b3b8ac7db129e3ab6963c3a564c16db
benny <vvsogi@gmail.com>
2024-10-10 14:50:12 +0900

    Add mock data for Category

---
+++ mock/mock.ts
@@ -0,0 +1,22 @@
+import { Category } from '@/app/types'
+
+export const mockCategory: Category[] = [
+  {
+    id: '1',
+    title: 'Mock 1',
+    updatedAt: new Date('2021-01-01'),
+    createdAt: new Date('2021-01-01')
+  },
+  {
+    id: '2',
+    title: 'Mock 2',
+    updatedAt: new Date('2021-01-01'),
+    createdAt: new Date('2021-01-01')
+  },
+  {
+    id: '3',
+    title: 'Mock 3',
+    updatedAt: new Date('2021-01-01'),
+    createdAt: new Date('2021-01-01')
+  }
+]
