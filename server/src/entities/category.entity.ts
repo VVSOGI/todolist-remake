ecc7826d99968ddb5175c4619bfb1f73dad7cd92
benny <vvsogi@gmail.com>
2024-10-11 15:52:36 +0900

    Add Category entity class

---
+++ server/src/entities/category.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryColumn({ type: 'uuid' })
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
