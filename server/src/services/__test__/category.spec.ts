0028e509a74f2b58b72e45a7c05fcef28ce77a7a
benny <vvsogi@gmail.com>
2024-10-11 16:10:33 +0900

    Add category unit tests

---
+++ server/src/services/__test__/category.spec.ts
@@ -0,0 +1,72 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryController, CategoryService } from '../category';
+import { Category } from 'src/entities/category.entity';
+import typia from 'typia';
+
+describe('CategoryModule', () => {
+  let controller: CategoryController;
+  let service: CategoryService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CategoryController],
+      providers: [
+        {
+          provide: CategoryService,
+          useValue: {
+            getCategories: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<CategoryController>(CategoryController);
+    service = module.get<CategoryService>(CategoryService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getCategories', () => {
+    it('should return Category type array', async () => {
+      const mockCategories: Category[] = [
+        {
+          id: '1',
+          title: 'Test Category 1',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: '2',
+          title: 'Test Category 2',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      // uuid가 아니어도 성공 수정 필요
+
+      jest.spyOn(service, 'getCategories').mockResolvedValue({
+        data: mockCategories,
+        total: mockCategories.length,
+      });
+
+      const result = (await controller.getCategories()).data;
+      console.log(result);
+      //   const [first, second] = result;
+
+      //   expect(first.id).toBe('1');
+      //   expect(second.id).toBe('2');
+    });
+
+    it('should return empty array', async () => {
+      jest.spyOn(service, 'getCategories').mockResolvedValue({
+        data: [],
+        total: 0,
+      });
+      const result = (await controller.getCategories()).data;
+      expect(result.length).toBe(0);
+    });
+  });
+});
--- server/src/services/category/category.repository.ts
+++ server/src/services/category/category.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from 'src/entities';
 import { Repository } from 'typeorm';
-import { Category } from 'src/entities/category.entity';
 
 @Injectable()
 export class CategoryRepository {
