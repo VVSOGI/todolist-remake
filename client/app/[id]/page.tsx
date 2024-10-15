7937636a2dcb3ca673bcb8685c6ed1ee04768ba4
benny <vvsogi@gmail.com>
2024-10-15 18:07:35 +0900

    Add CreateTodolist component to page

--- client/app/[id]/page.tsx
+++ client/app/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { styles } from '@/app/styles'
 import { UUID } from '@/app/types'
-import { Container, TodolistHeader, TodolistMain } from '@/app/ui'
+import { Container, CreateTodolist, TodolistHeader, TodolistMain } from '@/app/ui'
 import { getCategoryById, getTodolistByCategoryId } from '@/app/utils'
 
 interface Props {
@@ -22,6 +22,7 @@ export default async function page({ params: { id: categoryId } }: Props) {
     >
       <TodolistMain>
         <TodolistHeader category={category} />
+        <CreateTodolist />
       </TodolistMain>
     </Container>
   )
