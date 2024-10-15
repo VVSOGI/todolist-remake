f4318631a18c9b96a351314eb391466e101df2fd
benny <vvsogi@gmail.com>
2024-10-15 15:23:04 +0900

    Add TodolistMain component to UI

---
+++ client/app/ui/todolist/TodolistMain.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import { styles } from '@/app/styles'
+
+const Main = styled.main`
+  width: 970px;
+  height: 480px;
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+  border-radius: ${styles.borderRadius.medium};
+  background-color: ${styles.backgroundColor.default};
+  box-shadow: ${styles.boxShadow.primary};
+`
+
+interface Props {
+  children: React.ReactNode
+}
+
+export function TodolistMain({ children }: Props) {
+  return <Main>{children}</Main>
+}
