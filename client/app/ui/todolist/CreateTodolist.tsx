70c6183768f8d9b86e73ca648ca41b9b68c65ec7
benny <vvsogi@gmail.com>
2024-10-17 17:24:28 +0900

    Remove unused import in CreateTodolist.tsx

--- client/app/ui/todolist/CreateTodolist.tsx
+++ client/app/ui/todolist/CreateTodolist.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { styles } from '@/app/styles'
 import { Input, LargeButton } from '@/app/ui'
