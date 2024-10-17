3ebe0ad5c8cc2d66a6fe40026e43e60d165762b2
benny <vvsogi@gmail.com>
2024-10-17 16:06:24 +0900

    Refactor API routes to use fetchToBackend utility

--- client/app/api/todolist/[categoryId]/route.ts
+++ client/app/api/todolist/[categoryId]/route.ts
@@ -1,5 +1,6 @@
-import { UUID } from '@/app/types'
 import { NextResponse } from 'next/server'
+import { UUID } from '@/app/types'
+import { fetchToBackend } from '@/app/utils'
 
 interface Params {
   params: {
@@ -8,15 +9,13 @@ interface Params {
 }
 
 export async function GET(req: Request, { params: { categoryId } }: Params) {
-  const response = await fetch(`${process.env.BACKEND_SERVER_URL}/todolist/${categoryId}`, {
+  const data = await fetchToBackend(`/todolist/${categoryId}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     },
     cache: 'no-cache'
   })
-
-  const data = await response.json()
 
   return NextResponse.json(data, {
     status: 200
---
+++ client/app/api/todolist/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import { fetchToBackend } from '@/app/utils'
+
+export async function PATCH(req: Request) {
+  const body = await req.json()
+
+  const data = await fetchToBackend(`/todolist`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body),
+    cache: 'no-cache'
+  })
+
+  return NextResponse.json(data, {
+    status: 200
+  })
+}
