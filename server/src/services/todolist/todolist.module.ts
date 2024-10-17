3856ac249c708858f925e486aaf0dbe43fe86ebc
benny <vvsogi@gmail.com>
2024-10-17 17:48:07 +0900

    Add CategoryModule to TodolistModule imports and update TodolistService constructor

--- server/src/services/todolist/todolist.module.ts
+++ server/src/services/todolist/todolist.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Todolist } from 'src/entities/todolist.entity'
 import { TodolistController, TodolistService, TodolistRepository } from '.'
+import { CategoryModule } from '../'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Todolist])],
+  imports: [TypeOrmModule.forFeature([Todolist]), CategoryModule],
   controllers: [TodolistController],
   providers: [TodolistService, TodolistRepository]
 })
--- server/src/services/todolist/todolist.service.ts
+++ server/src/services/todolist/todolist.service.ts
@@ -3,19 +3,14 @@ import { v4 } from 'uuid'
 import { TodolistRepository } from './todolist.repository'
 import { CreateTodolistDto, UpdateTodolistDto, createTodolist } from './types'
 import { Todolist } from 'src/entities'
+import { CategoryService } from '../category'
 
 @Injectable()
 export class TodolistService {
-  constructor(private todolistRepository: TodolistRepository) {}
-
-  private async findTodoById(id: string) {
-    const target = await this.todolistRepository.findById(id)
-    if (target) {
-      return target
-    }
-
-    throw new NotFoundException(`The todolist you're looking for doesn't exist.`)
-  }
+  constructor(
+    private todolistRepository: TodolistRepository,
+    private categoryService: CategoryService
+  ) {}
 
   private async saveTodolist(todolist: Todolist) {
     return this.todolistRepository.save(todolist)
@@ -35,13 +30,23 @@ export class TodolistService {
 
   async updateTodolist(updateTodolist: UpdateTodolistDto) {
     const { id } = updateTodolist
-    const target = await this.findTodoById(id)
-    const updated = { ...target, ...updateTodolist }
+    const target = await this.getTodolistById(id)
+    const updated: Todolist = { ...target, ...updateTodolist, updatedAt: new Date() }
+    this.categoryService.updateDate(target.categoryId)
     return this.saveTodolist(updated)
   }
 
   async getTodolists() {
     return this.todolistRepository.findAll()
+  }
+
+  async getTodolistById(id: string) {
+    const target = await this.todolistRepository.findById(id)
+    if (target) {
+      return target
+    }
+
+    throw new NotFoundException(`The todolist you're looking for doesn't exist.`)
   }
 
   async getTodolistsByCategoryId(categoryId: string) {
