a7761eef33f73e6f75b80ec5cc0cd8f84be9fa7b
benny <vvsogi@gmail.com>
2024-10-22 17:26:02 +0900

    Add CustomIValidation interface to type.ts

--- server/src/common/type.ts
+++ server/src/common/type.ts
@@ -5,3 +5,23 @@ export interface TypiaAssertError {
   status: number
   options: any
 }
+
+export type CustomIValidation<T = unknown> = CustomIValidation.ISuccess<T> | CustomIValidation.IFailure
+
+export declare namespace CustomIValidation {
+  interface ISuccess<T = unknown> {
+    success: true
+    data: T
+    errors: []
+  }
+  interface IFailure {
+    success: false
+    errors: IError[]
+  }
+  interface IError {
+    path: string
+    expected: string
+    value: any
+    messages?: any
+  }
+}
