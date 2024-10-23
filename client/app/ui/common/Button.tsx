e10772655f6c892da40fa6278b102fcc12d3eb85
benny <vvsogi@gmail.com>
2024-10-23 16:40:16 +0900

    Refactor Button component to support different themes

--- client/app/ui/common/Button.tsx
+++ client/app/ui/common/Button.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import { colors } from '@/app/styles'
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<StylesProps>`
   width: fit-content;
   min-width: 48px;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: ${colors.red_600};
+  background-color: ${(props) => {
+    return props.stylestheme === ButtonsTheme.bright ? colors.red_600 : colors.gray_500
+  }};
   padding: 0 12px;
   color: ${colors.white};
   font-weight: 700;
@@ -17,23 +19,37 @@ const StyledButton = styled.button`
   user-select: none;
 
   &:hover {
-    background-color: ${colors.red_500};
+    background-color: ${(props) => {
+      return props.stylestheme === ButtonsTheme.bright ? colors.red_500 : colors.gray_400
+    }};
   }
 
   &:active {
-    background-color: ${colors.red_600};
+    background-color: ${(props) => {
+      return props.stylestheme === ButtonsTheme.bright ? colors.red_600 : colors.gray_500
+    }};
   }
 `
+
+export enum ButtonsTheme {
+  bright = 'BRIGHT',
+  dark = 'DARK'
+}
+
+interface StylesProps {
+  stylestheme: ButtonsTheme
+}
 
 interface Props {
   children: React.ReactNode
   style?: React.CSSProperties
+  stylesTheme?: ButtonsTheme
   onClick: () => void
 }
 
-export function Button({ children, style, onClick }: Props) {
+export function Button({ children, style, stylesTheme = ButtonsTheme.bright, onClick }: Props) {
   return (
-    <StyledButton style={style} onClick={onClick}>
+    <StyledButton stylestheme={stylesTheme} style={style} onClick={onClick}>
       {children}
     </StyledButton>
   )
