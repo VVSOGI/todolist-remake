8703850ac775bcf0306752e3af3a1bc5d78c7cf7
benny <vvsogi@gmail.com>
2024-10-23 16:40:34 +0900

    Add stylesTheme prop to LargeButton component

--- client/app/ui/common/LargeButton.tsx
+++ client/app/ui/common/LargeButton.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Button } from '.'
+import { Button, ButtonsTheme } from '.'
 
 interface Props {
   style?: React.CSSProperties
   children: React.ReactNode
+  stylesTheme?: ButtonsTheme
   onClick: () => void
 }
 
-export function LargeButton({ style, children, onClick }: Props) {
+export function LargeButton({ style, stylesTheme, children, onClick }: Props) {
   return (
-    <Button style={{ minWidth: '84px', ...style }} onClick={onClick}>
+    <Button stylesTheme={stylesTheme} style={{ minWidth: '84px', ...style }} onClick={onClick}>
       {children}
     </Button>
   )
