298865bb0fc3a887b89265844a23973b36a9c914
benny <vvsogi@gmail.com>
2024-10-24 15:53:10 +0900

    Add CategoryDeleteParamsDto type

--- server/src/services/category/types/params.type.ts
+++ server/src/services/category/types/params.type.ts
@@ -3,3 +3,5 @@ import { tags } from 'typia'
 export interface CategoryIdParamsDto {
   categoryId: string & tags.Format<'uuid'>
 }
+
+export type CategoryDeleteParamsDto = 'true' | 'false' | undefined
