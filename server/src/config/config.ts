0381971c5fcd11d4eb75f0df8719cc22d2288f67
benny <vvsogi@gmail.com>
2024-10-25 23:12:02 +0900

    Update server port configuration

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,8 +4,12 @@ import { getNumber, getString } from 'src/utils'
 dotenv.config({ path: '.env' })
 
 export const Config = {
+  server: {
+    port: getNumber('SERVER_PORT')
+  },
+
   db: {
-    host: getString('DB_HOST'),
+    host: getString('RUN_ENVIRONMENT') === 'local' ? getString('LOCAL_HOST') : getString('DOCKER_HOST'),
     port: getNumber('DB_PORT'),
     username: getString('DB_USERNAME'),
     password: getString('DB_PASSWORD'),
