bb4853260a491aa44efc64c4f44c67b1f9448527
benny <vvsogi@gmail.com>
2024-10-29 15:16:25 +0900

    Remove unused import in CategoryRepository

--- server/src/services/category/category.repository.ts
+++ server/src/services/category/category.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Category } from 'src/entities'
 import { Repository } from 'typeorm'
-import { CategoryDeleteParamsDto, CreateCategory } from './types'
+import { CreateCategory } from './types'
 
 @Injectable()
 export class CategoryRepository {
