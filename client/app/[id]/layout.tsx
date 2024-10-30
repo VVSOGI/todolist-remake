f123d80d84a9827f41e9205673001b0351a34bcb
benny <vvsogi@gmail.com>
2024-10-30 15:24:35 +0900

    Refactor layout and page components

--- client/app/[id]/layout.tsx
+++ client/app/[id]/layout.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Container } from '@/app/ui'
+import { styles } from '@/app/styles'
 
 export default function layout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return <div>{children}</div>
+  return (
+    <Container
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: styles.backgroundColor.primary
+      }}
+    >
+      {children}
+    </Container>
+  )
 }
--- client/app/[id]/page.tsx
+++ client/app/[id]/page.tsx
@@ -1,6 +1,5 @@
-import { styles } from '@/app/styles'
 import { UUID } from '@/app/types'
-import { Container, Todolist, TodolistHeader, TodolistMain } from '@/app/ui'
+import { Todolist, TodolistHeader, TodolistMain } from '@/app/ui'
 import { getCategoryById, getTodolistByCategoryId } from '@/app/utils'
 
 interface Props {
@@ -18,18 +17,9 @@ export default async function page({ params: { id: categoryId } }: Props) {
   }
 
   return (
-    <Container
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: styles.backgroundColor.primary
-      }}
-    >
-      <TodolistMain>
-        <TodolistHeader category={category} />
-        <Todolist categoryId={category.id} todolist={todolist.data} getTodolist={getTodolist} />
-      </TodolistMain>
-    </Container>
+    <TodolistMain>
+      <TodolistHeader category={category} />
+      <Todolist categoryId={category.id} todolist={todolist.data} getTodolist={getTodolist} />
+    </TodolistMain>
   )
 }
