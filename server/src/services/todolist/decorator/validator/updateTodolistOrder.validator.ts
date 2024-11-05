3107d815b288b0f2978a1472ac8055a2a5ec6003
benny <vvsogi@gmail.com>
2024-11-05 17:19:52 +0900

    Add updateTodolistOrder.validator.ts for validating the update of todolist order #7

---
+++ server/src/services/todolist/decorator/validator/updateTodolistOrder.validator.ts
@@ -0,0 +1,26 @@
+import typia from 'typia'
+import { BadRequestException } from '@nestjs/common'
+import { CustomIValidation } from 'src/common'
+import { UpdateTodolistRequest } from '..'
+import { UpdateTodolistOrderDto } from '../../types'
+import { Todolist } from 'src/entities'
+
+export class UpdateTodolistOrderValidator {
+  private body: Todolist[]
+
+  constructor(request: UpdateTodolistRequest) {
+    this.body = request.body
+  }
+
+  validate(): Todolist[] | never {
+    for (let i = 0; i < this.body.length; i++) {
+      const checkValidate: CustomIValidation<UpdateTodolistOrderDto> = typia.validateEquals<UpdateTodolistOrderDto>(this.body[i])
+      const { errors } = checkValidate
+      if (errors.length) {
+        throw new BadRequestException(errors[0])
+      }
+    }
+
+    return this.body
+  }
+}
