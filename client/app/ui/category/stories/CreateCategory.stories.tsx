4bd5bb9afce3f3e58b2eb71d4b8a735ba50cf077
benny <vvsogi@gmail.com>
2024-11-22 00:40:27 +0900

    Change ts file to  CreateCategory.stories.tsx, change css

---
+++ client/app/ui/category/stories/CreateCategory.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import { CreateCategory } from '@/app/ui'
+import { colors, styles } from '@/app/styles'
+
+const createCategory = {
+  title: 'Example/Button/UseCase',
+  component: CreateCategory,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['!autodocs'],
+  argTypes: {},
+  args: {
+    categoryTitle: '',
+    changeValue: fn(),
+    handleSubmit: fn()
+  },
+  decorators: (Story) => (
+    <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div
+        style={{
+          position: 'absolute',
+          maxWidth: '720px',
+          width: '100%',
+          height: '80vh'
+        }}
+      >
+        <Story />
+        <div
+          style={{
+            height: '50%',
+            border: `1px solid ${colors.gray_200}`,
+            borderRadius: styles.borderRadius.small
+          }}
+        />
+      </div>
+    </div>
+  )
+} satisfies Meta<typeof CreateCategory>
+
+export default createCategory
+type Story = StoryObj<typeof createCategory>
+
+export const StylesCreateCategory: Story = {
+  args: {}
+}
