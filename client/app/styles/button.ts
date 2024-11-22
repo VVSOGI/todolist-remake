aa20b2e6df2516b2a4b35419d993d98d8dfb2c67
benny <vvsogi@gmail.com>
2024-11-22 18:50:43 +0900

    Add button styles and sizes #64

---
+++ client/app/styles/button.ts
@@ -0,0 +1,74 @@
+import { css } from 'styled-components'
+import { colors } from '@/app/styles'
+import { RuleSet } from 'styled-components/dist/types'
+
+export enum ButtonsTheme {
+  BRIGHT = 'BRIGHT',
+  DARK = 'DARK'
+}
+
+export interface ButtonStyleProps {
+  stylestheme: ButtonsTheme
+  size: ButtonSize
+}
+
+export enum ButtonSize {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large'
+}
+
+export const ButtonDefault = css<ButtonStyleProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${colors.white};
+  cursor: pointer;
+  user-select: none;
+
+  background-color: ${(props) => {
+    return props.stylestheme === ButtonsTheme.BRIGHT ? colors.red_600 : colors.gray_500
+  }};
+
+  &:hover {
+    background-color: ${(props) => {
+      return props.stylestheme === ButtonsTheme.BRIGHT ? colors.red_500 : colors.gray_400
+    }};
+  }
+
+  &:active {
+    background-color: ${(props) => {
+      return props.stylestheme === ButtonsTheme.BRIGHT ? colors.red_600 : colors.gray_500
+    }};
+  }
+`
+
+export const ButtonSizes: Record<ButtonSize, RuleSet> = {
+  small: css`
+    padding: 0.8rem 0.4rem;
+    width: fit-content;
+    min-width: 3rem;
+    height: 100%;
+    min-height: 2rem;
+    font-size: 0.8rem;
+    font-weight: 600;
+  `,
+  medium: css`
+    padding: 1rem 0.6rem;
+    width: fit-content;
+    min-width: 5rem;
+    height: 100%;
+    min-height: 3rem;
+    font-size: 1rem;
+    font-weight: 700;
+  `,
+  large: css`
+    padding: 1.2rem 0.8rem;
+    width: fit-content;
+    min-width: 8rem;
+    height: 100%;
+    min-height: 4rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+  `
+}
