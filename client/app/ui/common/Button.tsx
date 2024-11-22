c8aa0ed45c6dcd2215cd2e2428c58e66bb3aba43
benny <vvsogi@gmail.com>
2024-11-22 18:53:25 +0900

    Refactor Button component styles #64

--- client/app/ui/common/Button.tsx
+++ client/app/ui/common/Button.tsx
@@ -1,56 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
-import { colors } from '@/app/styles'
+import { ButtonSizes, ButtonDefault, ButtonStyleProps, ButtonsTheme, ButtonSize } from '@/app/styles/button'
 
-const StyledButton = styled.button<StylesProps>`
-  width: fit-content;
-  min-width: 48px;
-  height: 100%;
-  min-height: 32px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => {
-    return props.stylestheme === ButtonsTheme.bright ? colors.red_600 : colors.gray_500
+const StyledButton = styled.button<ButtonStyleProps>`
+  ${ButtonDefault};
+  ${(props) => {
+    return ButtonSizes[props.size]
   }};
-  padding: 0 12px;
-  color: ${colors.white};
-  font-weight: 700;
-  cursor: pointer;
-  user-select: none;
-
-  &:hover {
-    background-color: ${(props) => {
-      return props.stylestheme === ButtonsTheme.bright ? colors.red_500 : colors.gray_400
-    }};
-  }
-
-  &:active {
-    background-color: ${(props) => {
-      return props.stylestheme === ButtonsTheme.bright ? colors.red_600 : colors.gray_500
-    }};
-  }
 `
-
-export enum ButtonsTheme {
-  bright = 'BRIGHT',
-  dark = 'DARK'
-}
-
-interface StylesProps {
-  stylestheme: ButtonsTheme
-}
 
 interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   children: React.ReactNode
+  size: ButtonSize
   style?: React.CSSProperties
-  stylesTheme?: ButtonsTheme
+  theme?: ButtonsTheme
   onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
-export function Button({ children, style, stylesTheme = ButtonsTheme.bright, onClick, ...rest }: Props) {
+export function Button({ children, size, style, theme = ButtonsTheme.BRIGHT, onClick, ...rest }: Props) {
   return (
-    <StyledButton stylestheme={stylesTheme} style={style} onClick={onClick} {...rest}>
+    <StyledButton size={size} stylestheme={theme} style={style} onClick={onClick} {...rest}>
       {children}
     </StyledButton>
   )
