407af5b3ed8ccdc0923a70a7af4c726a97ec8d56
benny <vvsogi@gmail.com>
2024-11-25 18:23:24 +0900

    Refactor layout components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,19 +1,11 @@
 import { CategoryList, Container, CategorySection, Title, CreateCategory } from '@/app/ui'
 import { getCategoryList } from '@/app/utils'
-import { styles } from '@/app/styles'
 
 export default async function page() {
   const { data } = await getCategoryList()
 
   return (
-    <Container
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: styles.backgroundColor.primary
-      }}
-    >
+    <Container>
       <CategorySection>
         <Title>{String('Make Your Own Business To-Do List').toUpperCase()}</Title>
         <CreateCategory />
