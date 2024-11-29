7b6567d8f2c32e60d00ddaf20528f7abdac39156
benny <vvsogi@gmail.com>
2024-11-29 22:33:51 +0900

    Refactor Todolist component and add DraggableTodolist #86

--- client/app/ui/todolist/Todolist.tsx
+++ client/app/ui/todolist/Todolist.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import { DndContext } from '@dnd-kit/core'
-import { SortableContext } from '@dnd-kit/sortable'
 import { Todo } from '@/app/types'
 import { SCROLL_BAR_SETTINGS, TODOLIST_HEIGHTS, colors } from '@/app/styles'
-import { CreateTodolist, SortableOverlay, TodoItem, TodoUpdateModal } from '@/app/ui'
-import { saveTodolistOrder, useDragDndKit, useTodolist, useTodolistEditModal } from '@/app/utils'
+import { CreateTodolist, DraggableTodolist, TodoUpdateModal } from '@/app/ui'
+import { useTodolist, useTodolistEditModal } from '@/app/utils'
 
 const TodolistWrapper = styled.div`
   height: calc(100% - (${TODOLIST_HEIGHTS.HEADER} + ${TODOLIST_HEIGHTS.CREATE_INPUT}));
   ${SCROLL_BAR_SETTINGS};
 `
 
-const NothingInList = styled.div`
+const EmptyTodolist = styled.div`
   position: relative;
   max-height: 2.8125rem;
   display: flex;
@@ -29,7 +27,6 @@ interface Props {
 
 export function Todolist({ categoryId, todolist, getTodolist }: Props) {
   const { list, setList, handleCompleteTodo, handleCreateTodo, handleEditTodo } = useTodolist({ categoryId, todolist, getTodolist })
-  const { activeItem, sensors, handleDragStart, handleDragEnd } = useDragDndKit<Todo>({ list, setList })
   const {
     modal,
     targetTodo,
@@ -49,31 +46,9 @@ export function Todolist({ categoryId, todolist, getTodolist }: Props) {
         />
       )}
 
-      <DndContext
-        sensors={sensors}
-        onDragStart={handleDragStart}
-        onDragEnd={(props) => handleDragEnd({ ...props, saveCallback: saveTodolistOrder })}
-      >
-        <SortableContext items={list}>
-          {list.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} handleCompleteTodo={handleCompleteTodo} handleEditModalOpen={handleEditModalOpen} />
-          ))}
-        </SortableContext>
-        <SortableOverlay>
-          {activeItem ? (
-            <TodoItem
-              key={activeItem.id}
-              todo={activeItem}
-              handleCompleteTodo={handleCompleteTodo}
-              handleEditModalOpen={handleEditModalOpen}
-            />
-          ) : null}
-        </SortableOverlay>
-      </DndContext>
-
-      {!list.length && <NothingInList>Nothing in list 😅</NothingInList>}
-
       <audio id="audio" src="/poped.wav"></audio>
+      {!list.length && <EmptyTodolist>Nothing in list 😅</EmptyTodolist>}
+      <DraggableTodolist list={list} setList={setList} handleCompleteTodo={handleCompleteTodo} handleEditModalOpen={handleEditModalOpen} />
       <CreateTodolist handleCreateTodo={handleCreateTodo} />
     </TodolistWrapper>
   )
