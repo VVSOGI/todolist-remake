b472ed0eff5bd310957f552ef8b89189f23ed097
benny <vvsogi@gmail.com>
2024-12-03 18:01:10 +0900

    Add description to CategoryItem component in CategoryItem.stories.tsx

--- client/app/stories/category/CategoryItem.stories.tsx
+++ client/app/stories/category/CategoryItem.stories.tsx
@@ -8,9 +8,15 @@ const categoryItem = {
   title: 'Example/Category/CategoryItem',
   component: CategoryItem,
   parameters: {
-    layout: 'center'
+    layout: 'center',
+    docs: {
+      description: {
+        component:
+          '투두리스트를 가진 카테고리 입니다. \n\n 해당 카테고리를 클릭하면 투두리스트 화면으로 이동합니다. \n\n 해당 카테고리는 드래그를 좌우로 이동하면 특정 기능을 가진 버튼이 나옵니다.'
+      }
+    }
   },
-  tags: ['!autodocs'],
+  tags: ['autodocs'],
   argTypes: {},
   args: {
     category: {
