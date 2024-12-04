19335a358ac7c5421ed3a581a811e8797a97024e
benny <vvsogi@gmail.com>
2024-12-04 16:07:09 +0900

    Add mock category data for Categories.stories.tsx

--- client/app/stories/category/Categories.stories.tsx
+++ client/app/stories/category/Categories.stories.tsx
@@ -1,29 +1,8 @@
 import { fn } from '@storybook/test'
 import { Categories, CategorySection } from '@/app/ui'
 import { colors } from '@/app/styles'
-import { Category } from '@/app/types'
 import type { Meta, StoryObj } from '@storybook/react'
-
-const mockCategories: Category[] = [
-  {
-    id: '1',
-    title: 'Test Title',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: '2',
-    title: 'Something new category',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: '3',
-    title: 'Is there a job what I can?',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-]
+import { mockCategories } from '@/app/stories/mock'
 
 const categories = {
   title: 'Example/Category/Categories',
