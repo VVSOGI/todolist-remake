ee352411acab76a34f5615fcddddd4d3f0f9d14d
benny <vvsogi@gmail.com>
2024-12-06 17:53:26 +0900

    Add TodolistPage component and its stories

---
+++ client/app/stories/todolist/usecases/TodolistPage.stories.tsx
@@ -0,0 +1,58 @@
+import { Container, TodolistDisplay, TodolistHeader, TodolistSection } from '@/app/ui'
+import { fn } from '@storybook/test'
+import { mockCategories } from '../../mock'
+import { mockTodoItems } from '../../mock/todolist'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const TodolistPage = {
+  title: 'Example/Todolist/UseCase/Page',
+  component: TodolistDisplay,
+  parameters: {
+    layout: 'fullscreen',
+    tags: ['!autodocs']
+  },
+  argTypes: {},
+  args: {
+    categoryId: mockCategories[0].id,
+    todolist: mockTodoItems,
+    getTodolist: fn()
+  },
+  decorators: (Story) => (
+    <Container>
+      <TodolistSection>
+        <TodolistHeader category={mockCategories[0]} />
+        <Story />
+      </TodolistSection>
+    </Container>
+  )
+} satisfies Meta<typeof TodolistDisplay>
+
+export default TodolistPage
+type Story = StoryObj<typeof TodolistPage>
+
+/**
+ * 가장 기본적인 형태의 TodoItem 입니다.
+ */
+export const Styles1Tablet: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'tablet'
+    }
+  }
+}
+
+export const Styles2Desktop: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'desktop'
+    }
+  }
+}
+
+export const Styles3Mobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'iphone14'
+    }
+  }
+}
