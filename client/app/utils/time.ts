325c872b6cf89e34800ddba8e8470bd9981485ca
benny <vvsogi@gmail.com>
2024-12-10 18:44:40 +0900

    Refactor changeToLocaleTime function to accept a change parameter

--- client/app/utils/time.ts
+++ client/app/utils/time.ts
@@ -1,4 +1,11 @@
-export function changeToLocaleTime(time: Date) {
-  const changed = new Date(time.toString()).toLocaleString('ko').split('. ').slice(0, 3).join('. ')
-  return changed
+export function changeToLocaleTime(time: Date, change: (data: Date) => string = changeToDate) {
+  return change(time)
+}
+
+export function changeToDate(time: Date): string {
+  return new Date(time.toString()).toLocaleString('ko').split('. ').slice(0, 3).join('. ')
+}
+
+export function changeToTime(time: Date): string {
+  return new Date(time.toString()).toLocaleString('ko')
 }
