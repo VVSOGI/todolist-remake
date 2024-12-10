20c9c154b7092ecd13666798a3612a22100811ab
benny <vvsogi@gmail.com>
2024-12-10 18:00:16 +0900

    Refactor changeToLocaleTime function to display only date and time in Korean locale

--- client/app/utils/time.ts
+++ client/app/utils/time.ts
@@ -1,4 +1,4 @@
 export function changeToLocaleTime(time: Date) {
-  const changed = new Date(time.toString()).toLocaleString('ko')
+  const changed = new Date(time.toString()).toLocaleString('ko').split('. ').slice(0, 3).join('. ')
   return changed
 }
