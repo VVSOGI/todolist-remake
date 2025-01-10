0786e520989af863fb7bf18fea82f67a53eaf64d
benny <vvsogi@gmail.com>
2025-01-10 17:33:03 +0900

    Add category components to stories package

---
+++ packages/stories/app/(main)/category/components/CategorySection.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import { BORDER_RADIUS_SIZES, BOX_SHADOWS, COLORS, COMMON_MEDIA_QUERY } from '@/app/styles'
+
+const Section = styled.section`
+  height: 30rem;
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem;
+  background-color: ${COLORS.WHITE};
+  ${BOX_SHADOWS.primary};
+  ${BORDER_RADIUS_SIZES.medium};
+  ${COMMON_MEDIA_QUERY.section};
+`
+
+interface Props {
+  children: React.ReactNode
+}
+
+export function CategorySection({ children }: Props) {
+  return <Section>{children}</Section>
+}
---
+++ packages/stories/app/(main)/category/components/index.ts
@@ -0,0 +1,8 @@
+'use client'
+
+export * from './CategorySection'
+export * from './CreateCategory'
+export * from './CategoryDisplay'
+export * from './CategoryUpdateModal'
+export * from './CategoryDeleteModal'
+export * from './Categories'
