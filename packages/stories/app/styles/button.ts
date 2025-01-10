6adc471d0c7d26c2e4aed550385a49f47a829a84
benny <vvsogi@gmail.com>
2025-01-10 17:37:28 +0900

    Move button styles and colors to packages/stories

---
+++ packages/stories/app/styles/button.ts
@@ -0,0 +1,59 @@
+import { css } from 'styled-components'
+import { RuleSet } from 'styled-components/dist/types'
+import { buttonSize, ButtonStyleProps, buttonsTheme } from '@/app/types'
+import { COLORS } from '@/app/styles'
+
+export const BUTTON_DEFAULT_STYLE = css<ButtonStyleProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${COLORS.WHITE};
+  cursor: pointer;
+  user-select: none;
+
+  background-color: ${(props) => {
+    return props.$stylestheme === buttonsTheme.BRIGHT ? COLORS.RED_600 : COLORS.GRAY_500
+  }};
+
+  &:hover {
+    background-color: ${(props) => {
+      return props.$stylestheme === buttonsTheme.BRIGHT ? COLORS.RED_500 : COLORS.GRAY_400
+    }};
+  }
+
+  &:active {
+    background-color: ${(props) => {
+      return props.$stylestheme === buttonsTheme.BRIGHT ? COLORS.RED_600 : COLORS.GRAY_500
+    }};
+  }
+`
+
+export const BUTTON_SIZES: Record<buttonSize, RuleSet> = {
+  small: css`
+    padding: 0.8rem 0.4rem;
+    width: fit-content;
+    min-width: 3rem;
+    height: 100%;
+    min-height: 2rem;
+    font-size: 0.8rem;
+    font-weight: 600;
+  `,
+  medium: css`
+    padding: 1rem 0.6rem;
+    width: fit-content;
+    min-width: 5rem;
+    height: 100%;
+    min-height: 2.5rem;
+    font-size: 1rem;
+    font-weight: 700;
+  `,
+  large: css`
+    padding: 1.2rem 0.8rem;
+    width: fit-content;
+    min-width: 8rem;
+    height: 100%;
+    min-height: 4rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+  `
+}
