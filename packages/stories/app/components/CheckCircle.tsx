645054b4dc727874f9386225ec5a014d7becb936
benny <vvsogi@gmail.com>
2025-01-10 17:33:38 +0900

    Add common components and files

---
+++ packages/stories/app/components/CheckCircle.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { COLORS } from '@/app/styles'
+import { GiCheckMark } from 'react-icons/gi'
+
+const Circle = styled.div`
+  width: 1.125rem;
+  height: 1.125rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${COLORS.WHITE};
+  border: 1px solid ${COLORS.GRAY_300};
+  border-radius: 50%;
+  cursor: pointer;
+
+  &:hover {
+    svg {
+      color: ${COLORS.RED_400};
+    }
+  }
+`
+
+const CheckIcon = styled(GiCheckMark)`
+  color: ${COLORS.WHITE};
+  font-size: 0.625rem;
+  transition: 0.2s;
+`
+
+interface Props {
+  onAnimationEnd: () => void
+}
+
+export function CheckCircle({ onAnimationEnd }: Props) {
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  const handleAnimate = () => {
+    setIsAnimating(true)
+  }
+
+  const handleAnimateStop = () => {
+    setIsAnimating(false)
+  }
+
+  return (
+    <Circle
+      className={isAnimating ? 'animate' : 'animate-stop'}
+      onClick={() => {
+        handleAnimate()
+      }}
+      onAnimationEnd={() => {
+        handleAnimateStop()
+        onAnimationEnd()
+      }}
+    >
+      <CheckIcon />
+    </Circle>
+  )
+}
---
+++ packages/stories/app/components/dnd-kit/SortableOverlay.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { DragOverlay, defaultDropAnimationSideEffects } from '@dnd-kit/core'
+import { BORDER_RADIUS_SIZES } from '@/app/styles'
+
+import type { PropsWithChildren } from 'react'
+import type { DropAnimation } from '@dnd-kit/core'
+
+const dropAnimationConfig: DropAnimation = {
+  sideEffects: defaultDropAnimationSideEffects({
+    styles: {
+      active: {
+        opacity: '0.1'
+      }
+    }
+  })
+}
+
+const DragOverlayContainer = styled(DragOverlay)`
+  height: fit-content;
+  overflow: hidden;
+  box-shadow: 0 0.3125rem 0.625rem rgba(0, 0, 0, 0.1);
+  ${BORDER_RADIUS_SIZES.small};
+`
+
+export function SortableOverlay({ children }: PropsWithChildren) {
+  return <DragOverlayContainer dropAnimation={dropAnimationConfig}>{children}</DragOverlayContainer>
+}
