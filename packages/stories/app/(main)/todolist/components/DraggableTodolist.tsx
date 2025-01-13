8d34cd33cc23246b1dd758f9e99ff9e9311a2a0e
benny <vvsogi@gmail.com>
2025-01-13 17:35:05 +0900

    Refactor draggable todolist component and add new hooks

--- packages/stories/app/(main)/todolist/components/DraggableTodolist.tsx
+++ packages/stories/app/(main)/todolist/components/DraggableTodolist.tsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import { DndContext } from '@dnd-kit/core'
-import { SortableContext } from '@dnd-kit/sortable'
-import { SortableOverlay } from '@/app/components'
-import { TodoItem } from '@/app/(main)/todolist/components'
-import { useDragDndKit } from '@/app/(main)/todolist/hooks'
-import { saveTodolistOrder } from '@/app/(main)/todolist/api'
-import { Todo } from '@/app/types'
+import React from "react";
+import { DndContext } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import { SortableOverlay } from "@/app/components";
+import { TodoItem } from "@/app/(main)/todolist/components";
+import { useDragDndKit } from "@/app/(main)/todolist/hooks";
+import { Todo } from "@/app/types";
 
 interface Props {
-  list: Todo[]
-  setList: (item: Todo[]) => void
-  handleCompleteTodo: (todo: Todo) => Promise<void>
-  handleEditModalOpen: (todo: Todo) => void
+  list: Todo[];
+  setList: (item: Todo[]) => void;
+  handleCompleteTodo: (todo: Todo) => Promise<void>;
+  handleEditModalOpen: (todo: Todo) => void;
 }
 
 export function DraggableTodolist({ list, setList, handleCompleteTodo, handleEditModalOpen }: Props) {
-  const { activeItem, sensors, handleDragStart, handleDragEnd } = useDragDndKit<Todo>({ list, setList })
+  const { activeItem, sensors, handleDragStart, handleDragEnd } = useDragDndKit<Todo>({ list, setList });
   return (
     <DndContext
       sensors={sensors}
       onDragStart={handleDragStart}
-      onDragEnd={(props) => handleDragEnd({ ...props, saveCallback: saveTodolistOrder })}
+      onDragEnd={(props) =>
+        handleDragEnd({
+          ...props,
+          saveCallback: (some) => {
+            return new Promise((res) => res(some));
+          },
+        })
+      }
     >
       <SortableContext items={list}>
         {list.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} handleCompleteTodo={handleCompleteTodo} handleEditModalOpen={handleEditModalOpen} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleCompleteTodo={handleCompleteTodo}
+            handleEditModalOpen={handleEditModalOpen}
+          />
         ))}
       </SortableContext>
       <SortableOverlay>
@@ -38,5 +49,5 @@ export function DraggableTodolist({ list, setList, handleCompleteTodo, handleEdi
         ) : null}
       </SortableOverlay>
     </DndContext>
-  )
+  );
 }
---
+++ packages/stories/app/(main)/todolist/hooks/index.ts
@@ -0,0 +1,5 @@
+'use client'
+
+export * from './useDragDndKit'
+export * from './useTodolist'
+export * from './useTodolistEditModal'
