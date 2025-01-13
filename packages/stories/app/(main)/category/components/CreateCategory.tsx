336aae318eee0bc3e755b2034c703c4fc2dcf289
benny <vvsogi@gmail.com>
2025-01-13 17:26:41 +0900

    Refactor CreateCategory component

--- packages/stories/app/(main)/category/components/CreateCategory.tsx
+++ packages/stories/app/(main)/category/components/CreateCategory.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { useRouter } from 'next/navigation'
-import { Button, Input } from '@/app/components'
-import { createCategory } from '@/app/(main)/category/api'
-import { BORDER_RADIUS_SIZES, COLORS, FONT_SIZES } from '@/app/styles'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useRouter } from "next/navigation";
+import { Button, Input } from "@/app/components";
+import { BORDER_RADIUS_SIZES, COLORS, FONT_SIZES } from "@/app/styles";
 
 const CreateCategoryWrapper = styled.div`
   width: 100%;
   height: fit-content;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Create = styled.div`
   width: 100%;
@@ -21,44 +20,43 @@ const Create = styled.div`
   border: 1px solid ${COLORS.GRAY_200};
   overflow: hidden;
   ${BORDER_RADIUS_SIZES.small}
-`
+`;
 
 const CreateError = styled.p`
   margin: 0.5625rem 0;
   font-size: ${FONT_SIZES.xs};
   color: ${COLORS.RED_600};
-`
+`;
 
 export function CreateCategory() {
-  const [categoryTitle, setCategoryTitle] = useState('')
-  const [error, setError] = useState('')
+  const [categoryTitle, setCategoryTitle] = useState("");
+  const [error, setError] = useState("");
 
-  const router = useRouter()
+  const router = useRouter();
 
   const changeValue = (value: string) => {
-    setCategoryTitle(value)
-  }
+    setCategoryTitle(value);
+  };
 
-  const handleSubmit = async (value: string) => {
+  const handleSubmit = async () => {
     try {
-      setError('')
-      setCategoryTitle('')
-      await createCategory({ title: value })
-      router.refresh()
+      setError("");
+      setCategoryTitle("");
+      router.refresh();
     } catch (e: any) {
-      setError(JSON.parse(e.message).message)
+      setError(JSON.parse(e.message).message);
     }
-  }
+  };
 
   return (
     <CreateCategoryWrapper>
       <Create>
         <Input handleSubmit={handleSubmit} changeValue={changeValue} value={categoryTitle} />
-        <Button size="small" onClick={() => handleSubmit(categoryTitle)}>
+        <Button size="small" onClick={() => handleSubmit()}>
           +
         </Button>
       </Create>
       <CreateError>{error}</CreateError>
     </CreateCategoryWrapper>
-  )
+  );
 }
