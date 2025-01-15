37d03485d9205290d7a34c2120074309b8028649
benny <vvsogi@gmail.com>
2025-01-15 18:01:38 +0900

    Add server-side flag to API functions

--- packages/client/app/(main)/category/page.tsx
+++ packages/client/app/(main)/category/page.tsx
@@ -1,25 +1,16 @@
 import { Title } from '@/app/components'
 import { CategoryDisplay, CategorySection, CreateCategory } from '@/app/(main)/category/components'
-import { createCategory, getCategoryList } from '@/app/(main)/category/api'
+import { createCategory, deleteCategory, getCategoryList, updateCategory } from '@/app/(main)/category/api'
 
 export default async function page() {
   const { response } = await getCategoryList()
   const { data } = response
 
-  const create = async (title: string) => {
-    'use server'
-    try {
-      return await createCategory({ title })
-    } catch (e: any) {
-      throw e
-    }
-  }
-
   return (
     <CategorySection>
       <Title>{String('Make Your Own Business To-Do List').toUpperCase()}</Title>
-      <CreateCategory createCategory={create} />
-      {data && <CategoryDisplay categories={data} />}
+      <CreateCategory createCategory={createCategory} />
+      {data && <CategoryDisplay categories={data} updateCategory={updateCategory} deleteCategory={deleteCategory} />}
     </CategorySection>
   )
 }
