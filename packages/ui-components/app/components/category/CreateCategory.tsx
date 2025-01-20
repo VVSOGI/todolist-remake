af68d8a7dc045f0bb4a5eafc5472ebb59d58a804
benny <vvsogi@gmail.com>
2025-01-20 18:45:33 +0900

    Refactor CreateCategory component and remove unused files

--- packages/ui-components/app/components/category/CreateCategory.tsx
+++ packages/ui-components/app/components/category/CreateCategory.tsx
@@ -1,32 +1,8 @@
+"use client";
+
 import React, { useState } from "react";
-import styled from "styled-components";
 import { useRouter } from "next/navigation";
-import { Button, Input } from "@/app/components";
 import { BORDER_RADIUS_SIZES, COLORS, FONT_SIZES } from "@/app/styles";
-
-const CreateCategoryWrapper = styled.div`
-  width: 100%;
-  height: fit-content;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Create = styled.div`
-  width: 100%;
-  height: 2.5rem;
-  min-height: 2.5rem;
-  display: flex;
-  justify-content: space-between;
-  border: 1px solid ${COLORS.GRAY_200};
-  overflow: hidden;
-  ${BORDER_RADIUS_SIZES.small}
-`;
-
-const CreateError = styled.p`
-  margin: 0.5625rem 0;
-  font-size: ${FONT_SIZES.xs};
-  color: ${COLORS.RED_600};
-`;
 
 interface Props {
   createCategory: (body: { title: string }) => Promise<{
@@ -57,14 +33,11 @@ export function CreateCategory({ createCategory }: Props) {
   };
 
   return (
-    <CreateCategoryWrapper>
-      <Create>
-        <Input handleSubmit={handleSubmit} changeValue={changeValue} value={categoryTitle} />
-        <Button size="small" onClick={() => handleSubmit(categoryTitle)}>
-          +
-        </Button>
-      </Create>
-      <CreateError>{error}</CreateError>
-    </CreateCategoryWrapper>
+    <div>
+      <div
+        className={`flex w-full min-h-[2.5rem] h-[2.5rem] justify-between border border-[${COLORS.GRAY_200}] overflow-hidden rounded-[0.25rem]`}
+      ></div>
+      <div className={`my-[0.5625rem]`}>{error}</div>
+    </div>
   );
 }
