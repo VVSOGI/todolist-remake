0c2079dbf40ea8bc1e586b5c4335c7c6f57778b5
benny <vvsogi@gmail.com>
2025-01-23 18:14:29 +0900

    Refactor category layout and loading components

--- packages/client/app/(main)/category/loading.tsx
+++ packages/client/app/(main)/category/loading.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { Container } from '@/app/components'
-import { Skeleton, CreateCategory, Title } from '@todolist/ui-components/app'
-import { CategorySection } from './components'
+import { Skeleton, CreateCategory } from '@todolist/ui-components/app'
 
 export default function HomeLoading() {
   async function mock() {
@@ -10,26 +8,23 @@ export default function HomeLoading() {
   }
 
   return (
-    <Container>
-      <CategorySection>
-        <Title>{String('Make Your Own Business To-Do List').toUpperCase()}</Title>
-        <CreateCategory createCategory={mock} />
-        <div
-          style={{
-            width: '100%',
-            height: '60%',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '0.5rem'
-          }}
-        >
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-        </div>
-      </CategorySection>
-    </Container>
+    <div>
+      <CreateCategory createCategory={mock} />
+      <div
+        style={{
+          width: '100%',
+          height: '60%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '0.5rem'
+        }}
+      >
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </div>
+    </div>
   )
 }
