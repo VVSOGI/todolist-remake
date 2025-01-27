00cb84aa1e9a013f47c141db91ecf1d7a6c69471
benny <vvsogi@gmail.com>
2025-01-27 17:39:27 +0900

    Refectoring styles types import paths, Delete unused code

--- packages/ui-components/app/styles/button.ts
+++ packages/ui-components/app/styles/button.ts
@@ -1,6 +1,11 @@
-import { buttonSize, buttonsTheme } from "../types";
+export enum ButtonsTheme {
+  BRIGHT = "bright",
+  DARK = "dark",
+}
 
-export const BUTTON_DEFAULT_STYLES: Record<buttonsTheme, string> = {
+export type ButtonSize = "small" | "medium" | "large";
+
+export const BUTTON_DEFAULT_STYLES: Record<ButtonsTheme, string> = {
   bright: `
     flex justify-center items-center bg-red-600 select-none cursor-pointer text-white
     hover:bg-red-500
@@ -14,7 +19,7 @@ export const BUTTON_DEFAULT_STYLES: Record<buttonsTheme, string> = {
   `,
 };
 
-export const BUTTON_SIZES_STYLES: Record<buttonSize, string> = {
+export const BUTTON_SIZES_STYLES: Record<ButtonSize, string> = {
   small: "w-fit min-w-[3rem] h-full min-h-[2rem] py-[0.8rem] px-[0.4rem] text-[0.8rem] font-[600]",
   medium: "w-fit min-w-[5rem] h-full min-h-[2.5rem] py-[1em] px-[0.6rem] text-[1rem] font-[700]",
   large: "w-fit min-w-[8rem] h-full min-h-[4rem] py-[1.2rem] px-[0.8rem] text-[1.2rem] font-[700]",
--- packages/ui-components/app/styles/mediaQueries.ts
+++ packages/ui-components/app/styles/mediaQueries.ts
@@ -1,16 +1,10 @@
-import { commonComponents } from "@/app/types";
+type Components = "section";
 
-export const COMMON_MEDIA_QUERY_STYLES: Record<commonComponents, string> = {
+export const COMMON_MEDIA_QUERY_STYLES: Record<Components, string> = {
   section: `
     relative w-full h-full bg-white rounded-medium overflow-hidden
     mobile:flex mobile:flex-col mobile:rounded-none mobile:shadow-primary
     tablet:w-section-tablet tablet:h-[30rem]
     desktop:w-section-desktop
-  `,
-
-  title: `
-    mobile:text-sm
-    tablet:text-md
-    desktop:text-lg
   `,
 };
