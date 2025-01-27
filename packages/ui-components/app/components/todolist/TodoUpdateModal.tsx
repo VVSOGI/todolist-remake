c14046a96fab3b2f4f7cf187fbadf3933ddd8d76
benny <vvsogi@gmail.com>
2025-01-27 22:36:34 +0900

    Add TodoUpdateModal component

---
+++ packages/ui-components/app/components/todolist/TodoUpdateModal.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { AgreementModal, Input } from "..";
+
+interface Props {
+  placeholder?: string;
+  updateTitle: string;
+  handleAgree: () => void;
+  handleRefuse: () => void;
+  setUpdateTitle: (updated: string) => void;
+}
+
+export function TodoUpdateModal({ placeholder, updateTitle, setUpdateTitle, handleAgree, handleRefuse }: Props) {
+  return (
+    <AgreementModal title="Edit" handleAgree={handleAgree} handleRefuse={handleRefuse}>
+      <div className="w-full py-[1.5rem] px-0 flex flex-col gap-[0.75rem]">
+        <div>Change Todo Title</div>
+        <Input
+          className="w-full border border-gray-200 rounded-small"
+          value={updateTitle}
+          changeValue={(value) => setUpdateTitle(value)}
+          handleSubmit={() => {}}
+          placeholder={placeholder}
+        />
+      </div>
+    </AgreementModal>
+  );
+}
