261a8ea16ba82770c2e2fca81c16fb442efea3b2
benny <vvsogi@gmail.com>
2025-01-28 19:07:22 +0900

    Refactor storage page and layout components

--- packages/client/app/(main)/storage/loading.tsx
+++ packages/client/app/(main)/storage/loading.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Container } from '@todolist/ui-components/app'
-import { StorageHeader, StorageSection } from '@/app/(main)/storage/components'
+import { StorageHeader } from '@/app/(main)/storage/components'
 
 export default function loading() {
   return (
-    <Container>
-      <StorageSection>
-        <StorageHeader
-          category={{
-            id: '',
-            title: '...Loading',
-            updatedAt: new Date('2000.01.01'),
-            createdAt: new Date('2000.01.01')
-          }}
-        />
-      </StorageSection>
-    </Container>
+    <>
+      <StorageHeader
+        category={{
+          id: '',
+          title: '...Loading',
+          updatedAt: new Date('2000.01.01'),
+          createdAt: new Date('2000.01.01')
+        }}
+      />
+    </>
   )
 }
