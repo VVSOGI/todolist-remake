48faca899bece257e3cb7f4908e064aa3961e1d4
benny <vvsogi@gmail.com>
2025-01-31 17:37:20 +0900

    Remove unused import statements and reorganize props in Button component

--- packages/ui-components/app/components/common/Button.tsx
+++ packages/ui-components/app/components/common/Button.tsx
@@ -4,14 +4,14 @@ import React from "react";
 import { BUTTON_DEFAULT_STYLES, BUTTON_SIZES_STYLES, ButtonsTheme, ButtonSize } from "../../styles";
 
 interface Props {
-  children?: React.ReactNode;
   size: ButtonSize;
+  children?: React.ReactNode;
   style?: React.CSSProperties;
   theme?: ButtonsTheme;
   onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
-export function Button({ children, size, style, theme = ButtonsTheme.BRIGHT, onClick, ...rest }: Props) {
+export function Button({ size, children, style, theme = ButtonsTheme.BRIGHT, onClick, ...rest }: Props) {
   return (
     <button
       className={`${BUTTON_DEFAULT_STYLES[theme]} ${BUTTON_SIZES_STYLES[size]}`}
