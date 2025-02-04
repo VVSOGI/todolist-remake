6b7c118f7711d4a761341820196b2eaaa00fe6a5
benny <vvsogi@gmail.com>
2025-02-04 18:26:15 +0900

    Update Storybook configuration and add font

--- packages/stories/.storybook/preview.ts
+++ packages/stories/.storybook/preview.ts
@@ -1,47 +1,47 @@
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-import { mediaQueryStandard } from '../app/types'
-import '../app/globals.css'
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { mediaQueryStandard } from "../app/types";
+import "../app/globals.css";
 
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
 
 const customViewports = {
   desktop: {
-    name: 'Desktop',
+    name: "Desktop",
     styles: {
       width: mediaQueryStandard.DESKTOP,
-      height: '100%'
-    }
+      height: "100%",
+    },
   },
 
   tablet: {
-    name: 'Tablet',
+    name: "Tablet",
     styles: {
-      width: '1200px',
-      height: '35rem'
-    }
-  }
-}
+      width: "1200px",
+      height: "35rem",
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
     options: {
       storySort: (a, b) => {
-        return a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true })
-      }
+        return a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true });
+      },
     },
     nextjs: {
-      appDirectory: true
+      appDirectory: true,
     },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i
-      }
+        date: /Date$/i,
+      },
     },
     viewport: {
-      viewports: { ...INITIAL_VIEWPORTS, ...customViewports }
-    }
-  }
-}
+      viewports: { ...INITIAL_VIEWPORTS, ...customViewports },
+    },
+  },
+};
 
-export default preview
+export default preview;
