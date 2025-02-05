fc9ec07b07c2e9d85d80fdfce822771eea37cd1c
benny <vvsogi@gmail.com>
2025-02-05 17:18:30 +0900

    Refactor code structure and fix import statements

--- packages/stories/app/utils/common.ts
+++ packages/stories/app/utils/common.ts
@@ -1,52 +1,20 @@
-'use client'
-
 /** MouseEvent */
 
-import { ColorsByGroup, ColorGroup } from '@/app/types'
-import { COLORS } from '@/app/styles'
-
-interface DragHorizonProps {
-  event: MouseEvent | React.MouseEvent<HTMLButtonElement, MouseEvent>
-  leftCallback: () => void
-  rightCallback: () => void
-}
-
-export function dragHorizon({ event, leftCallback, rightCallback }: DragHorizonProps) {
-  const start = event.pageX
-
-  const mouseMoveHandler = (e: MouseEvent) => {
-    const end = e.pageX
-    if (start - end > 5) {
-      leftCallback()
-      return
-    }
-
-    if (start - end < -5) {
-      rightCallback()
-      return
-    }
-  }
-
-  const mouseUpHandler = () => {
-    document.removeEventListener('mousemove', mouseMoveHandler)
-  }
-
-  document.addEventListener('mousemove', mouseMoveHandler)
-  document.addEventListener('mouseup', mouseUpHandler, { once: true })
-}
+import { ColorsByGroup, ColorGroup } from "@/app/types";
+import { COLORS } from "@/app/styles";
 
 /** Time */
 
 export function changeToLocaleTime(time: Date, change: (data: Date) => string = changeToDate) {
-  return change(time)
+  return change(time);
 }
 
 export function changeToDate(time: Date): string {
-  return new Date(time.toString()).toLocaleString('ko').split('. ').slice(0, 3).join('. ')
+  return new Date(time.toString()).toLocaleString("ko").split(". ").slice(0, 3).join(". ");
 }
 
 export function changeToTime(time: Date): string {
-  return new Date(time.toString()).toLocaleString('ko')
+  return new Date(time.toString()).toLocaleString("ko");
 }
 
 /** Colors */
@@ -57,8 +25,8 @@ export function getColorsByGroup<T extends ColorGroup>(color: T): ColorsByGroup<
     .reduce(
       (acc, [key, value]) => ({
         ...acc,
-        [key]: value
+        [key]: value,
       }),
       {} as ColorsByGroup<T>
-    )
+    );
 }
