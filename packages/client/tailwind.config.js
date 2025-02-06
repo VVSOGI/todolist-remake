6a31d6760e278dd33d1439f1929f77b57b7a74f2
benny <vvsogi@gmail.com>
2025-02-06 18:22:31 +0900

    Add additional path to tailwind.config.js

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@vvsogi/ui-components/app/**/*.{js,jsx,ts,tsx}'
+    './node_modules/@vvsogi/ui-components/app/**/*.{js,jsx,ts,tsx}',
+    '../../node_modules/@vvsogi/ui-components/app/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     screens: {
