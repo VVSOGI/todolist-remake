4ff93218320f9035fa28b5fa72f694c78425ea59
benny <vvsogi@gmail.com>
2025-02-10 21:19:53 +0900

    Add Swagger documentation to the server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Config } from './config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,25 @@ async function bootstrap() {
       origin: Config.client.url
     }
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('Make free todolist')
+    .setDescription(
+      `A full-stack todo list application designed for practice, focusing on improving architectural skills and testing various design patterns (container-presenter, layered ...) <br /> 
+      in both frontend and backend development.
+      `
+    )
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1
+    }
+  })
+
   await app.listen(Config.server.port)
 }
 bootstrap()
