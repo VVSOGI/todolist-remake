d9c63fb231eb51af3795a3fddc9f084caf43d207
benny <vvsogi@gmail.com>
2025-02-10 21:59:40 +0900

    Add ApiResponse import to CreateCategory.swagger.ts

--- packages/server/src/services/category/decorator/swagger/createCategory.swagger.ts
+++ packages/server/src/services/category/decorator/swagger/createCategory.swagger.ts
@@ -1,5 +1,5 @@
 import { applyDecorators } from '@nestjs/common'
-import { ApiBody, ApiProperty } from '@nestjs/swagger'
+import { ApiBody, ApiProperty, ApiResponse } from '@nestjs/swagger'
 
 class CreateCategory {
   @ApiProperty({
@@ -7,7 +7,6 @@ class CreateCategory {
     description: `Decide on a title for the category you want to create`
   })
   title: string
-  description: string
 }
 
 export function SwaggerCreateCategory() {
@@ -19,44 +18,53 @@ export function SwaggerCreateCategory() {
 You must attach a title to the body as a required DTO.
 
 ## If you sent invalid type data [400 ERROR]
-    POST 
-        {
-            title: 1000,
-        }
+POST 
 
-    Response
-        {
-            "message": "Received unexpected data 'title' [INVALID TYPE ERROR]",
-            "error": "Bad Request",
-            "statusCode": 400
-        }
+    {
+        title: 1000,
+    }
+
+Response
+
+    {
+        "message": "Received unexpected data 'title' [INVALID TYPE ERROR]",
+        "error": "Bad Request",
+        "statusCode": 400
+    }
 
 ## If you sent less than three characters [400 ERROR]
-    POST 
-        {
-            title: "",
-        }
+POST 
 
-    Response
-        {
-            "message": "You must enter at least three characters. [WRONG DATA SENT ERROR]",
-            "error": "Bad Request",
-            "statusCode": 400
-        }
+    {
+        title: "",
+    }
+
+Response
+
+    {
+        "message": "You must enter at least three characters. [WRONG DATA SENT ERROR]",
+        "error": "Bad Request",
+        "statusCode": 400
+    }
 
 ## If you sent missing data [404 ERROR]
-    POST 
-        { NOTHING }
 
-    Response
-        {
-            "message": "Received unexpected data 'title' [MISSING DATA ERROR]",
-            "error": "Not Found",
-            "statusCode": 404
-        }
+POST 
 
-        
-      `
+    { NOTHING }
+
+Response
+
+    {
+        "message": "Received unexpected data 'title' [MISSING DATA ERROR]",
+        "error": "Not Found",
+        "statusCode": 404
+    }
+`
+    }),
+    ApiResponse({
+      status: 201,
+      description: 'success'
     })
   )
 }
