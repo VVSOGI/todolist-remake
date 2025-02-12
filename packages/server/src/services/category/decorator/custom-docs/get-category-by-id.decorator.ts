a7bc69bf0494aca1f4a32a3c69b61ca600aacdad
benny <vvsogi@gmail.com>
2025-02-12 18:09:23 +0900

    Add custom documentation decorator for getting category by ID

---
+++ packages/server/src/services/category/decorator/custom-docs/get-category-by-id.decorator.ts
@@ -0,0 +1,55 @@
+import { ApiDocsEndpoint, EndpointDecoratorMetadata } from 'src/common'
+
+export const DocsGetCategoryById = () => {
+  const metadata: EndpointDecoratorMetadata<{
+    params: 'categoryId'
+    response: 'id' | 'title' | 'createdAt' | 'updatedAt' | 'deleted'
+  }> = {
+    description: '',
+    request: {
+      params: {
+        type: 'string',
+        properties: {
+          categoryId: {
+            type: 'string',
+            description: 'Target category Id that you find'
+          }
+        }
+      }
+    },
+    response: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string',
+          description: 'Found category ID'
+        },
+        title: {
+          type: 'string',
+          description: 'Found category title'
+        },
+        createdAt: {
+          type: 'string',
+          description: 'Found category createdAt'
+        },
+        updatedAt: {
+          type: 'string',
+          description: 'Found category updatedAt'
+        },
+        deleted: {
+          type: 'boolean',
+          description: 'Found category deleted'
+        }
+      },
+      example: {
+        id: '16874008-8915-4d53-9239-3913f7ee2089',
+        title: 'Test title',
+        createdAt: '2025-02-10T13:00:27.440Z',
+        updatedAt: '2025-02-10T13:00:27.440Z',
+        deleted: false
+      }
+    }
+  }
+
+  return ApiDocsEndpoint(metadata)
+}
